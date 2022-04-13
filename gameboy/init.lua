0a26407750fbb637ad0931e9bcc565ccbbe5e32a
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-04-13 20:55:10 +0100

    Update init.lua

--- gameboy/init.lua
+++ gameboy/init.lua
@@ -87,12 +87,12 @@ function Gameboy:run_until_vblank()
   while self.io.ram[self.io.ports.LY] == 144 and instructions < 100000 do
     self:step()
     instructions = instructions + 1
-    if instructions % 30000 == 0 then task.wait() end
+    if instructions % 20000 == 0 then task.wait() end
   end
   while self.io.ram[self.io.ports.LY] ~= 144 and instructions < 100000  do
     self:step()
     instructions = instructions + 1
-    if instructions % 30000 == 0 then task.wait() end
+    if instructions % 20000 == 0 then task.wait() end
   end
   --self.audio.update()
 end
